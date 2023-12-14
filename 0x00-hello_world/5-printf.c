b41ec08a2a78225dc24c02e91a70f44d132774c7
Benedict <bobuzytech@gmail.com>
2023-12-14 09:10:21 -0800

    Print a sentence followed with a new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Print a sentence follwed by a new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+
+	return (0);
+}
