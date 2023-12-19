88dc2a8671fb9bf0f2a10a5801fc211859a94d68
Benedict <bobuzytech@gmail.com>
2023-12-19 09:44:26 -0800

    Fix and make the file betty compliant

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,12 +2,10 @@
 
 /**
  * print_last_digit - Prints the last digit of a number
- * @n - The number to be checked
+ * @n: The number to be checked
  *
  * Return: The last digit
  */
-
-int _abs(int n);
 
 int print_last_digit(int n)
 {
