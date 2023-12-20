f29cc41aa76b1e88f98a76e6de3dbff7a117bd38
Benedict <bobuzytech@gmail.com>
2023-12-20 13:45:21 -0800

    Create a header file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - This checks if a number is postive or negative
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
