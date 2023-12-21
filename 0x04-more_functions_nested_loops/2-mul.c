246bb3a58610437303bb8f12e5a6a8d8112ef782
Benedict <bobuzytech@gmail.com>
2023-12-21 04:04:30 -0800

    Return the product of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers
+ * @a: The first digit
+ * @b: The second digit
+ *
+ * Return: The product
+ */
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+
+	return (product);
+}
