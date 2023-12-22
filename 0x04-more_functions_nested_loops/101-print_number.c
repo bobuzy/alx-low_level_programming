14e90465ec8194e66030f33e5702f590cea2e654
Benedict <bobuzytech@gmail.com>
2023-12-21 18:24:35 -0800

    Print a number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - Print a number
+ * @n: The integer it prints
+ *
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	unsigned int m;
+
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+
+	m = n;
+	
+	if (m >= 10)
+	{
+		print_number(m / 10);
+	}
+	_putchar((m % 10) + '0');
+}
