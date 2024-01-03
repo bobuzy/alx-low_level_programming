570fda9cb8eaa53983cf0675747c8355d2c39b2d
Benedict <bobuzytech@gmail.com>
2024-01-02 17:31:19 -0800

    Reduce the lines in the function

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _atoi - Converts a string to an integer
- * @s: the pointer to be converted
+ * _atoi - Convert a string to an integer
+ * @s: The pointer string to be converted
  *
  * Return: An integer
  */
@@ -19,39 +19,29 @@ int _atoi(char *s)
 	digit = 0;
 
 	while (s[len] != '\0')
-	{
 		len++;
-	}
 
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
-		{
 			++d;
-		}
 
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
 			if (d % 2)
-			{
 				digit = -digit;
-			}
 			n = n * 10 + digit;
 			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
-			{
 				break;
-			}
 			f = 0;
 		}
 		i++;
 	}
 
 	if (f == 0)
-	{
 		return (0);
-	}
 
 	return (n);
 }
