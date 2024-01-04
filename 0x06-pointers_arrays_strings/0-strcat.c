00d135214ed6c5cf9db9c8304ca41b993262ac2a
Benedict <bobuzytech@gmail.com>
2024-01-04 13:06:52 -0800

    Concatenate two strings together

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strcat - Join two strings together
+ * @dest: The destination string
+ * @src: The source string
+ *
+ * Return: This returns the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int n;
+	int m;
+
+	n = 0;
+	while (dest[n] != '\0')
+	{
+		n++;
+	}
+
+	m = 0;
+	while (src[m] != '\0')
+	{
+		dest[n] = src[m];
+		n++;
+		m++;
+	}
+
+	dest[n] = '\0';
+	return (dest);
+}
