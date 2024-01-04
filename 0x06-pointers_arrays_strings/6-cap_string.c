d3299d1da2f3d2549f69ef3be05b27daf1cbcdf1
Benedict <bobuzytech@gmail.com>
2024-01-04 13:53:11 -0800

    Capitalize all words in a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to be capitalized.
+ *
+ * Return: A pointer to the changed string.
+ */
+
+char *cap_string(char *str)
+{
+	int n = 0;
+
+	while (str[n])
+	{
+		while (!(str[n] >= 'a' && str[n] <= 'z'))
+			n++;
+
+		if (str[n - 1] == ' ' ||
+		    str[n - 1] == '\t' ||
+		    str[n - 1] == '\n' ||
+		    str[n - 1] == ',' ||
+		    str[n - 1] == ';' ||
+		    str[n - 1] == '.' ||
+		    str[n - 1] == '!' ||
+		    str[n - 1] == '?' ||
+		    str[n - 1] == '"' ||
+		    str[n - 1] == '(' ||
+		    str[n - 1] == ')' ||
+		    str[n - 1] == '{' ||
+		    str[n - 1] == '}' ||
+		    n == 0)
+			str[n] -= 32;
+
+		n++;
+	}
+
+	return (str);
+}
