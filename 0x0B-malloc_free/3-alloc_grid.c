f04eb7af55e1bd95508c8bedd8c500ecc8814c4a
Benedict <bobuzytech@gmail.com>
2024-01-16 11:42:14 -0800

    Create and return memory for a 2D array and initalize with 0

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * alloc_grid - Allocate memory for a 2D array
+ * @width: The width of the array
+ * @height: The height of the array
+ *
+ * Return: The pointer to the allocated memory of the 2D array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **ptr;
+	int i, j;
+	int n = 0, m = 0;
+
+	/* Allocate memories to store int pointers for each row */
+	/* This would create a pointer to a pointer */
+	ptr = (int **)malloc(height * sizeof(int *));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		/* Allocate a block of int memory to each row*/
+		ptr[i] = malloc(width * sizeof(int));
+
+		if (ptr[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				/* Free already craeated memories */
+				free(ptr[j]);
+			}
+			return (NULL);
+		}
+	}
+
+	while (n < height)
+	{
+		while (m < width)
+		{
+			ptr[n][m] = 0;
+			m++;
+		}
+		n++;
+	}
+
+	return (ptr);
+}
