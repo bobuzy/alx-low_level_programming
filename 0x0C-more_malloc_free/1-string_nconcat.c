ec32ff87eccd75c85be0c4e3b2f72763cf50f671
Benedict <bobuzytech@gmail.com>
2024-01-19 09:47:03 -0800

    Concatenate two strings by n bytes

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,42 +11,60 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0, j = 0, k = 0, l = 0;
-	char *m;
+	unsigned int i = 0, len1 = 0, len2 = 0, index = 0;
+	char *ptr;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+	s1 = check(s1);
+	s2 = check(s2);
 
-	while (s1[i])
-		i++;
-
-	while (s2[k])
-		k++;
-
-	if (n >= k)
-		l = i + k;
-	else
-		l = i + n;
-
-	m = malloc(sizeof(char) * l + 1);
-	if (m == NULL)
-		return (NULL);
-
-	k = 0;
-	while (j < l)
+	while (s1[len1])
 	{
-		if (j <= i)
-			m[j] = s1[j];
-
-		if (j >= i)
-		{
-			m[j] = s2[k];
-			k++;
-		}
-		j++;
+		len1++;
 	}
-	m[j] = '\0';
-	return (m);
+	while (s2[len2])
+	{
+		len2++;
+	}
+
+	if (n >= len2)
+	{
+		n = len2;
+	}
+	ptr = malloc((sizeof(char) * (len1 + n)) + 1);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	while (s1[length] != '\0')
+	{
+		ptr[length] = s1[length];
+		length++;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		ptr[length] = s2[i];
+		length++;
+	}
+
+	ptr[length] = '\0';
+	return (ptr);
+}
+
+/**
+ * check - This function treats a NULL string
+ * @str: The string to be checked
+ *
+ * Return: An empty string
+ */
+
+char *check(char *str)
+{
+	if (str == NULL)
+	{
+		str = "";
+	}
+	return (str);
 }
