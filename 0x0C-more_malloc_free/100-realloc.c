4bbfdfdc3b9b009c18e812538994d168f07440f9
Benedict <bobuzytech@gmail.com>
2024-01-20 05:08:28 -0800

    Reallocate memory using malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -5,44 +5,100 @@
   * @ptr: The pointer to the previously allocated memory block
   * @old_size: The current size of the memory block pointed by ptr
   * @new_size: The new size of the memory block after reallocation
+  *
   * Return: A pointer to the newly allocated memory block after reallocation
   */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *ptrn;
-	unsigned int i;
+	char *more, *less, *newm;
 
 	if (new_size == old_size)
+	{
 		return (ptr);
-
+	}
 	if (ptr == NULL)
 	{
-		ptrn = malloc(new_size);
-
-		if (ptrn == NULL)
-			return (NULL);
-
-		return (ptrn);
-	}
-	else
-	{
-		if (new_size == 0)
+		newm = malloc(new_size);
+		if (newm == NULL)
 		{
-			free(ptr);
 			return (NULL);
 		}
+		return (newm);
 	}
-
-	ptrn = malloc(new_size);
-
-	if (ptrn == NULL)
-		return (NULL);
-
-	for (i = 0; i < old_size && i < new_size; i++)
+	if (new_size == 0 && ptr != NULL)
 	{
-		ptrn[i] = ((char *) ptr)[i];
+		free(ptr);
+		return (NULL);
 	}
 
-	free(ptr);
+	if (new_size > old_size)
+	{
+		more = addmem(ptr, old_size, new_size);
+		return (more);
+	}
+	else if (new_size < old_size)
+	{
+		less = redmem(ptr, new_size);
+		return (less);
+	}
+	return (0);
+}
 
+
+/**
+ * addmem - Add more memory to the previously allocated memory
+ * @mem: Pointer to the previously allocated memory
+ * @prev: The size of the current memory
+ * @new: The new size of memory to be allocated
+ *
+ * Return: Pointer to the newly allocated memory
+ */
+
+char *addmem(void *mem, unsigned int prev, unsigned int newm)
+{
+	char *ptr;
+	unsigned int i;
+
+        ptr = malloc(newm);
+
+        if (ptr == NULL)
+        {
+                return (NULL);
+        }
+
+	for (i = 0; i < prev; i++)
+	{
+		((char *)ptr)[i] = ((char *)mem)[i];
+	}
+	return (ptr);
+}
+
+
+/**
+ * redmem - Reduce memory of the previously allocated memory
+ * @mem: Pointer to the previously allocated memory
+ * @prev: The size of the current memory
+ * @new: The new size of memory to be allocated
+ *
+ * Return: Pointer to the newly allocated memory
+ */
+
+char *redmem(void *mem, unsigned int newm)
+{
+        char *ptr;
+        unsigned int i;
+
+        ptr = malloc(newm);
+
+        if (ptr == NULL)
+        {
+                return (NULL);
+        }
+
+        for (i = 0; i < newm; i++)
+        {
+                ((char *)ptr)[i] = ((char *)mem)[i];
+        }
+        return (ptr);
+}
