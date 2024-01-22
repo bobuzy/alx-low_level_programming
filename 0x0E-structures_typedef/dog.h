8809495525824f40d1bd6eaba5cdbcf7a9517ac2
Benedict <bobuzytech@gmail.com>
2024-01-22 15:21:01 -0800

    Add comment description for struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+/**
+ * struct dog - Initialize name, age and owner variable for dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The name of the owner of the dog
+ */
+
 struct dog
 {
 	char *name;
