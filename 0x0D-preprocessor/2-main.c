fce9702592089046f068961330da431cf397ed76
Benedict <bobuzytech@gmail.com>
2024-01-22 11:27:58 -0800

    Print the name of the source file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define PROGRAMNAME __FILE__
+
+/**
+ * main - Print the name of the current file
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", PROGRAMNAME);
+
+	return (0);
+}
