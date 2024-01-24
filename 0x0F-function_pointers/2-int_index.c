1eade0a05b59fc63b54ceff63224f9649fe7ff47
Benedict <bobuzytech@gmail.com>
2024-01-24 13:18:15 -0800

    Add function description

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,5 +1,15 @@
 #include "function_pointers.h"
 
+/**
+  * int_index - Searche for an integer in an array
+  * and return the index
+  * @array: The array to be searched
+  * @size: The size of the array
+  * @cmp: The pointer to the function to be used to compare values
+  *
+  * Return: -1 if no element matches or if size < or = 0, else return i
+  */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
