8f87a1468fea2acbc849914d19b8bf7f90a84edf
Benedict <bobuzytech@gmail.com>
2024-01-24 12:33:09 -0800

    Callback a function to print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+  * print_name - This function callsback another function to
+  * print a name
+  * @name: The name to be printed
+  * @f: The pointer to the function
+  *
+  * Return: void
+  */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
+}
