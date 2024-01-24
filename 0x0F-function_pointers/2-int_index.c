8219e9a06327cf268fb955304e25a66da02fd2e6
Benedict <bobuzytech@gmail.com>
2024-01-24 12:59:03 -0800

    Search for an integer and return the index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array != NULL && cmp != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+			{
+				return (i);
+			}
+		}
+	}
+	return (0);
+}
