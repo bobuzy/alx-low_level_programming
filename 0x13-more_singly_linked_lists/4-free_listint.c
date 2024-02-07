9d1cdc66fae8aff731f6ddd45ebfa65fbd951cc8
Benedict <bobuzytech@gmail.com>
2024-02-06 19:53:51 -0800

    Add comment description

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+  * free_listint - This function frees a linked list.
+  * @head: The pointer to the first node of linked list
+  *
+  * Return: void
+  */
+
 void free_listint(listint_t *head)
 {
 	if (head == NULL)
