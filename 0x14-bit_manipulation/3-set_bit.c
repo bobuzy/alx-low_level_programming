bf0dcdf7db8500b702b03971d0ca90196f2fbfb9
Benedict <bobuzytech@gmail.com>
2024-02-08 16:04:20 -0800

    Set the value of a bit to 1 at a given index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  * set_bit - Set the value of a bit to 1 at a given index
+  * @n: The pointer to the number to be changed
+  * @index: The index where the bit would be chnaged to 1
+  *
+  * Return: 1, if successful. 0, otherwise.
+  */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int counter = 0, switcher = 1;
+
+	if (n == NULL)
+	{
+		return (-1);
+	}
+
+	while (counter < index)
+	{
+		switcher = switcher << 1;
+		counter++;
+	}
+
+	*n = *n | switcher;
+
+	return (1);
+}
