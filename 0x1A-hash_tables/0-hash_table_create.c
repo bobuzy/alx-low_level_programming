5731921a0e61c783b872eef34ac6d9c63b82fc80
Benedict <bobuzytech@gmail.com>
2024-04-13 12:23:31 -0700

    Remove execess free statements

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,7 +15,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (table == NULL)
 	{
-		free(table);
 		return (NULL);
 	}
 
@@ -24,7 +23,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (table->array == NULL)
 	{
-		free(table->array);
+		free(table)
 		return (NULL);
 	}
 
