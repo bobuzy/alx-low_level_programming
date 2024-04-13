2eb8c39e22b708c76c09fec97b2f119a18b1e820
Benedict <bobuzytech@gmail.com>
2024-04-13 08:57:58 -0700

    Create a hash_table_t type table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+
+/**
+ * hash_table_create - Create a hash table
+ * @size: The size of the hash table to be created
+ *
+ * Return: The hash table created
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	int ind, rev;
+	hash_table_t *table = malloc(sizeof(*hash_table_t));
+
+	if (table == NULL)
+	{
+		free(table);
+		return;
+	}
+
+	table->size = size;
+	table->array = (**hash_node_t) calloc(table->size, sizeof(*hash_node_t));
+
+	if (table->array == NULL)
+	{
+		free(table->array);
+		return;
+	}
+
+	for (ind = 0; ind < table->size; ind++)
+	{
+		table->array[ind] = NULL
+	}
+
+	return (table);
+}
