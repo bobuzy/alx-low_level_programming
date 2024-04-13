45b26be54bc8ff285c6abf12b272607603d8a374
Benedict <bobuzytech@gmail.com>
2024-04-13 15:33:03 -0700

    Add an element to the hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,79 @@
+#include "hash_tables.h"
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *node, *item;
+
+	if (!key || !ht || !value)
+	{
+		return (0);
+	}
+
+	index = hash_djb2((unsigned char *)key) % ht->size;
+
+	node = create_node(key, value);
+	if (node == NULL)
+	{
+		return (0);
+	}
+
+	item = ht->array[index];
+	if (item == NULL)
+	{
+		ht->array[index] = node;
+		return (1);
+	}
+	else if (strcmp(item->key, key) == 0)
+	{
+		free(item->value);
+		item->value = strdup(value);
+		if (value != NULL && item->value == NULL)
+		{
+			return (0);
+		}
+
+		free(node);
+		return (1);
+	}
+	else
+	{
+		node->next = item;
+		ht->array[index] = node;
+		return (1);
+	}
+}
+
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	if (key == NULL)
+	{
+		return (NULL);
+	}
+
+	node = malloc(sizeof(hash_node_t));
+
+	if (node == NULL)
+	{
+		return (NULL);
+	}
+
+	node->key = strdup(key);
+	node->value = strdup(value);
+
+	if (node->key == NULL)
+	{
+		return (NULL);
+	}
+
+	if (value != NULL && node->value == NULL)
+	{
+		return (NULL);
+	}
+
+	node->next = NULL;
+
+	return (node);
+}
